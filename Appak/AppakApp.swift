2f6bb0aa4be17529f1b5455e074fca63326d99e8
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-03-19 20:55:25 +0100

    Initial Commit

---
+++ Appak/AppakApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppakApp.swift
+//  Appak
+//
+//  Created by Aitor Zubizarreta Perez on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
