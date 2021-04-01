8713d7922f28924e6a4f8cf97cff851a48dd85c3
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-01 22:39:35 +0200

    Added 2 buttons in "PhoneAppDetailView", one to open a preview of an app from the App Store and the other to go to the App Store.

--- Appak/Views/PhoneAppDetailView.swift
+++ Appak/Views/PhoneAppDetailView.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
+import StoreKit // SKOverlay, SKStoreProductViewController
 
 struct PhoneAppDetailView: View {
+    
+    // MARK: - Properties
+    
+    @State private var showRecommended = false
+    
+    // MARK: - Methods
+    
+    // MARK: - View
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Show App Store Banner") {
+                showRecommended.toggle()
+            }
+            .appStoreOverlay(isPresented: $showRecommended) {
+                SKOverlay.AppConfiguration(appIdentifier: "871799243", position: .bottom)
+            }
+            Button(action: {
+                if let url = URL(string: "https://apps.apple.com/es/app/bizkaibus/id871799243"),
+                   UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url, options: [:]) { (success) in
+                        if success {
+                            print("Success")
+                        }
+                    }
+                }
+            }) {
+                Text("Go to App Store")
+            }
+        }
     }
 }
 
