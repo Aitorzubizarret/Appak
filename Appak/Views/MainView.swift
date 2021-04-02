c3cdff3b6d5fea269f442f69ed5e8a30d065962a
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-02 22:05:01 +0200

    Created the new view "PhoneAppCreateView". Updated "MainView" to add a button in the NavBar to open the new view to create a PhoneApp.

--- Appak/Views/MainView.swift
+++ Appak/Views/MainView.swift
@@ -12,6 +12,7 @@ struct MainView: View {
     // MARK: - Properties
     
     @EnvironmentObject var phoneAppsViewModel: PhoneAppsViewModel
+    @State private var showingPhoneAppCreateView: Bool = false
     
     // MARK: - Methods
     
@@ -30,6 +31,17 @@ struct MainView: View {
             }
             .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
             .navigationBarTitle("Appak", displayMode: .automatic)
+            .navigationBarItems(
+                trailing:
+                    Button(action: {
+                        self.showingPhoneAppCreateView.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                            .font(Font.system(size: 20, weight: Font.Weight.bold, design: Font.Design.default))
+                    }.sheet(isPresented: $showingPhoneAppCreateView, content: {
+                        PhoneAppCreateView()
+                    })
+            )
         }
     }
 }
