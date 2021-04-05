f551197b8075715453d5e0d6d2e616f9af41b1fb
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-04-05 16:42:20 +0200

    Updated "PhoneAppCreateView". Now it checks the introduced URL and if it is correct it created a new PhoneApp object and saves it in Core Data.

--- Appak/Views/PhoneAppCreateView.swift
+++ Appak/Views/PhoneAppCreateView.swift
@@ -13,7 +13,11 @@ struct PhoneAppCreateView: View {
     
     @Binding var showingPhoneAppCreateView: Bool
     @EnvironmentObject var phoneAppsViewModel: PhoneAppsViewModel
+    @State private var AppStoreURLChecked: Bool = false
     @State private var fieldAppStoreURL: String = ""
+    @State private var phoneAppName: String = ""
+    @State private var phoneAppURL: String = ""
+    @State private var phoneAppImageURL: String = ""
     
     // MARK: - Methods
     
@@ -22,26 +26,38 @@ struct PhoneAppCreateView: View {
     }
     
     ///
-    /// Create a PhoneApp with the URL
+    /// Check the App Store URL introduced by the user.
     ///
-    private func createPhoneAppSaveInViewModel() {
-        // Check the is data in Field.
+    private func checkIntroducedAppStoreURL() -> Bool {
+        var response: Bool = false
+        
+        // Check the field is not empty and the introduced array is a valid URL.
         guard self.fieldAppStoreURL != "",
-              let url: URL = URL(string: self.fieldAppStoreURL) else { return }
+              let url: URL = URL(string: self.fieldAppStoreURL) else { return response }
+        
+        // Check the URL is an App Store URL.
+        //
+        // - https://apps.apple.com/es/app/navegador-firefox/id989804926
+        // - https://apps.apple.com/es/app/id989804926
+        //
+        let urlParts = self.fieldAppStoreURL.components(separatedBy: "/")
+        guard urlParts[urlParts.count - 3] == "app",
+              urlParts[urlParts.count - 5] == "apps.apple.com" else { return response }
         
         self.getDataFromURL(url) { (success, iconURLString)  in
             if success && (iconURLString != "") {
-                let urlParts = self.fieldAppStoreURL.components(separatedBy: "/")
-                let phoneAppName: String = urlParts[urlParts.count - 2].capitalized
                 
-                self.phoneAppsViewModel.createNewPhoneApp(name: phoneAppName, urlString: self.fieldAppStoreURL)
+                self.phoneAppName = urlParts[urlParts.count - 2].capitalized
+                self.phoneAppURL = self.fieldAppStoreURL
+                self.phoneAppImageURL = iconURLString
                 
-                // Dismiss the view.
-                self.showingPhoneAppCreateView = false
+                response = true
             } else {
-                print("Error createPhoneAppSaveInViewModel - Success -> false")
+                print("Error checkIntroducedAppStoreURL - Success -> false")
             }
         }
+        
+        return response
     }
     
     ///
@@ -62,17 +78,38 @@ struct PhoneAppCreateView: View {
         }
     }
     
+    ///
+    /// Create a PhoneApp with the URL
+    ///
+    private func createPhoneAppSaveInViewModel() {
+        self.phoneAppsViewModel.createNewPhoneApp(name: self.phoneAppName, urlString: self.phoneAppURL)
+        
+        // Dismiss the view.
+        self.showingPhoneAppCreateView = false
+    }
+    
     // MARK: - View
     
     var body: some View {
         NavigationView {
             ScrollView {
                 Field(title: "App Store URL", value: self.$fieldAppStoreURL, keyboardType: .URL)
-                Button(action: {
-                    self.createPhoneAppSaveInViewModel()
-                }) {
-                    Text("Save App")
+                if !self.AppStoreURLChecked {
+                    Button(action: {
+                        if self.checkIntroducedAppStoreURL() {
+                            self.AppStoreURLChecked.toggle()
+                        }
+                    }) {
+                        Text("Check URL")
+                    }
+                } else {
+                    Button(action: {
+                        self.createPhoneAppSaveInViewModel()
+                    }) {
+                        Text("Add App to Appak")
+                    }
                 }
+                
             }
             .navigationBarTitle(Text("New App"), displayMode: .inline)
         }
